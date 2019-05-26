f81320d506348193f9ea399c31f46155f35b15a4
Dennis Mullen <dkmullen@gmail.com>
2019-05-26 19:02:20 -0400

    Two more

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
   email: {
@@ -11,5 +12,8 @@ const userSchema = mongoose.Schema({
     required: true
   }
 });
+
+// 'unique' in the schema isn't a validator, so we use this plugin
+userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('User', userSchema);
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 const router = express.Router();
 
-router.post('/signup');
+router.post('/signup', (req, res, next) => {
+
+});
 
 module.exports = router;
